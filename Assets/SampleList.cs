e9be9641bbdf3e666fdb1bd75cac9135055a327b
tvanvier-GMU <tvanvier@gmu.edu>
2016-10-30 00:11:07 -0400

    Test

--- Assets/SampleList.cs
+++ Assets/SampleList.cs
@@ -5,12 +5,17 @@ public class SampleList : MonoBehaviour {
 	//Use arrays only in very specific circumstances
 	public int[][] testArray = new int[2][];
 
+	public int[,] array = new int[1,1];
+
 	// Use this for initialization
 	void Start () {
 		//Arrays are not initialized with values already in them, this includes arrays of arrays.
 		testArray [0] = new int[3];
 		testArray [0] [1] = 1;
 		Debug.Log (testArray [0][1]);
+
+		array [0, 0] = 3;
+		Debug.Log (array [0, 0]);
 	}
 	
 	// Update is called once per frame
